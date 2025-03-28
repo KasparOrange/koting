d4af16fb99bad3d805ef988d1dfcfcb6477e17dc
Nico-Bao <nico.zingerle@gmail.com>
2025-03-28 23:37:09 +0100

    -

--- Bao/minor/tests.js
+++ Bao/minor/tests.js
@@ -1,8 +1,15 @@
-console.log((x => x % 2 === 0)(11));
+// console.log((x => x % 2 === 0)(11));
 
-myFunction = x => (x * 2)
-console.log(myFunction(10))
+// myFunction = x => (x * 2)
+// console.log(myFunction(10))
 
 const numArr = [4, 1, -20, -7, 5, 9, -6];
 
-numArr.forEach(element => {console.log(element)});
+// numArr.forEach(element => {console.log(element)});
+
+
+numArr.forEach(element => {
+    if (element > 0 || element % 2 === 0) {
+        console.log(element);
+    }
+});
