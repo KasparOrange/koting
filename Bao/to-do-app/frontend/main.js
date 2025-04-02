67cae355c0b03f67fa99dd91b8a09a719b137aa4
Nico-Bao <nico.zingerle@gmail.com>
2025-04-02 20:20:30 +0200

    fetchTasks() & addTask()

--- Bao/to-do-app/frontend/main.js
+++ Bao/to-do-app/frontend/main.js
@@ -1,80 +1,38 @@
-try {
-    const response = await fetch('http://127.0.0.1:3000/api/items');
+const input = document.querySelector('#new-task');
+const button = document.querySelector('#add-task');
+const list = document.querySelector('#task-list');
+const url = 'http://127.0.0.1:3000/api/items';
+
+const fetchTasks = async () => {
+    const response = await fetch(url);
     const items = await response.json();
 
-    const taskList = document.querySelector('#task-list');
-
     items.forEach(item => {
-        const newLiElement = document.createElement('li');
-
-        const newTextElement = document.createElement('p');
-
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.addEventListener('click', () => {
-            deleteItem(item._id);
-            newLiElement.remove();
-        });
-        newLiElement.appendChild(deleteButton);
-
-        newTextElement.textContent = item.text;
-
-        newLiElement.appendChild(newTextElement);
-
-        taskList.appendChild(newLiElement);
+        // shortest version but XSS risk, slower;
+        // alternative: .createElement & .appendChild
+        list.innerHTML += `<li><span>${item.text}</span></li>`;
     });
+};
+fetchTasks();
 
-    console.log(items);
-} catch (error) {
-    console.error('Error fetching tasks:', error);
-}
+const addTask = async () => {
+    const text = input.value;
+    input.value = '';
+    list.innerHTML += `<li><span>${text}</span></li>`;
+    
+    const response = await fetch(url, {
+        method: 'POST',
+        // This tells the server that you're sending JSON data in the request body.
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text }) // Send the new item as JSON
+    });
+};
+button.addEventListener('click', addTask);
 
-export async function addItem() {
-    const text = document.getElementById('new-task').value;
-
-    try {
-        const response = await fetch(`http://127.0.0.1:3000/api/items/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ text }) // Send the new item as JSON
-        });
-
-        if (!response.ok) {
-            throw new Error(`Server error: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        console.log('Created item:', data);
-    } catch (error) {
-        console.error('Error creating item:', error);
-    }
-}
-
-window.addItem = addItem;
-
-async function deleteItem(id) {
-    try {
-        const response = await fetch(`http://127.0.0.1:3000/api/items/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`Server error: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        console.log('Created item:', data);
-    } catch (error) {
-        console.error('Error creating item:', error);
-    }
-}
+// deleteTask
+// completeTask
 
 // add another sentence to the message element, change its style, and remove it (DOM practice)
 const messageAdjunct = document.createTextNode(' It was fun to make :)');
@@ -86,29 +44,3 @@ messageAdjunct.remove();
 // remove it again (longer variant)
 // messageElement.removeChild(messageElement.childNodes[0]);
 
-// #region MDN version to update shopping list (without database)
-// const list = document.querySelector('ul');
-// const input = document.querySelector('input');
-// const button = document.querySelector('button');
-
-// button.addEventListener('click', () => {
-//   const myItem = input.value;
-//   input.value = '';
-
-//   const listItem = document.createElement('li');
-//   const listText = document.createElement('span');
-//   const listBtn = document.createElement('button');
-
-//   listItem.appendChild(listText);
-//   listText.textContent = myItem;
-//   listItem.appendChild(listBtn);
-//   listBtn.textContent = 'Delete';
-//   list.appendChild(listItem);
-
-//   listBtn.addEventListener('click', () => {
-//     list.removeChild(listItem);
-//   });
-
-//   input.focus();
-// });
-// #endregion
