84c362165d69a387c1fffad8ccef91caf4d1ae08
Nico-Bao <174931404+Nico-Bao@users.noreply.github.com>
2025-05-27 20:10:32 +0200

    files that changed after run

--- Bao/BlazorSolution/obj/Debug/net9.0/BlazorProject.AssemblyInfo.cs
+++ Bao/BlazorSolution/obj/Debug/net9.0/BlazorProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ccfb4a921eb0a28ce78b19f18c2a2511835b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c9eaa93fbb8b7f1eb3bf4db691f1b3ae10af77")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
