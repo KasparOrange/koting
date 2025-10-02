dde30bc64bc7b8be0f810311ed94c8dfe237651d
Nico-Bao <174931404+Nico-Bao@users.noreply.github.com>
2025-10-02 15:46:15 +0200

    Add coloured headers for the datagrids, add MudAppBar & MudDrawer

--- Bao/BlazorSolution/Data/DbContext.cs
+++ Bao/BlazorSolution/Data/DbContext.cs
@@ -5,7 +5,7 @@ namespace BlazorProject.Data;
 public class DbContext {
     private readonly IMongoDatabase _database; // MongoDB database instance
 
-    public DbContext(IConfiguration configuration) { // Constructor takes an IConfiguration object
+    public DbContext() { // Constructor takes an IConfiguration object
         var client = new MongoClient("mongodb://localhost:27017"); // A MongoClient is instantiated using the connection string. This client handles the connection to the MongoDB server.
         _database = client.GetDatabase("PortfolioDemo"); // The GetDatabase method is called on the client, passing the name of the database. This method returns an instance of IMongoDatabase, which is then stored in our _database field
     }
