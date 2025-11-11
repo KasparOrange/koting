82a9c3c36eae636fb9f4d78578b70ef800e02214
Nico-Bao <174931404+Nico-Bao@users.noreply.github.com>
2025-11-11 19:34:26 +0100

    Add MudForm to DTCreatePortfolio, remove unnecessary DTO from DTCreateAsset,

--- Bao/BlazorSolution/Services/PageRoutes.cs
+++ Bao/BlazorSolution/Services/PageRoutes.cs
@@ -1,7 +1,7 @@
 namespace BlazorProject.Services;
 
 public class PageRoutes {
-    public const string Portfolios = "/portfolios";
-    public const string Edit_Asset_Type = "/";
+    public const string Portfolios = "/";
+    public const string Edit_Asset_Type = "/edit-asset-type";
     public const string Counter = "/counter";
 }
